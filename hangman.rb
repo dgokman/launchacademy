0afc43e2131a475f1565cca46944f12d4061ad27
Dennis Gokman <dennis.gokman@gmail.com>
2014-08-15 12:02:49 -0400

    Added guessing functionality

--- hangman.rb
+++ hangman.rb
@@ -1,6 +1,40 @@
 #!/usr/bin/env ruby
+require "pry"
+puts "Welcome to Hangman!"
 
-puts "Program goes here..."
 
-words = ["store", "persist", "magical", "hounds", "thirsty", "astute"]
+ words = ["store", "persist", "magical", "hounds", "thirsty", "astute"]
+ letters = []
+ chances = 8
+ word = words.sample
+
+ puts "Word: " + word
+ puts "Chances remaining: #{chances}"
+ puts "Guess a single letter (a-z) or the entire word:"
+ guess = gets.chomp.downcase
+ count = word.count(guess)
+
+while chances > 1
+ if word.include?(guess) == true
+  chances -= 1
+  letters << word
+  puts "Word: " + word
+  puts "Chances remaining: #{chances}"
+  puts "Guess a single letter (a-z) or the entire word:"
+  guess = gets.chomp.downcase
+  puts "Found #{count} occurrence(s) of the character #{guess}"
+ elsif word.include?(guess) == false
+  chances -=1
+  puts "Word: " + word
+  puts "Chances remaining: #{chances}"
+  puts "Guess a single letter (a-z) or the entire word:"
+  guess = gets.chomp.downcase
+  puts "Sorry, no #{guess}'s found."
+ end
+end
+
+
+
+
+
 
