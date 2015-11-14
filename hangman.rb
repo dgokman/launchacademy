f549611bbe7bab9ef444ac656bf588e3ffe02dc8
Dennis Gokman <dennis.gokman@gmail.com>
2015-11-14 13:22:36 -0500

    fixed hangman game

--- hangman.rb
+++ hangman.rb
@@ -1,54 +1,30 @@
-#!/usr/bin/env ruby
-require "pry"
-
-def get_guess
-  puts "Guess a single letter (a-z) or the entire word:"
+def get_guess(length)
+  puts "The word is #{length} letters. Guess a single letter (a-z) or the entire word:"
   gets.chomp.downcase
 end
 
 puts "Welcome to Hangman!"
 
+words = File.read('mobydick1.txt').split(" ").delete_if {|x| x.split("").any? {|y| y !~ /[a-zA-Z]/} || x.length < 5}
 
- words = ["store"]
-
- letters_hidden = []
- chances = 8
- word = words.sample
- letters = word.split("")
- # puts "Word: " + word
- # puts "Chances remaining: #{chances}"
- # puts "Guess a single letter (a-z) or the entire word:"
- guess = ""
- #count = ""
- #count = word.count(guess)
-
-while chances > 0
- if letters.include?(guess) == true
-
-
-    puts "Word: #{letters[-1]}" + "_" * (word.count(word) - 1)
-
-    puts "Chances remaining: #{chances}"
-    guess = get_guess
-    puts "Found #{count} occurrence(s) of the character #{guess}"
-    chances -= 1
-  else
-   puts "Word: " + "_"*word.count(word)
-
-   puts "Chances remaining: #{chances}"
-   guess = get_guess
-   puts "Found #{count} occurrence(s) of the character #{guess}"
-   chances -= 1
+word = words.sample
+partial_word = "_" * word.length
+guesses = 5
+while guesses >= 0
+  guess = get_guess(word.length)
+  if guess.length == 1 && word.include?(guess)
+    partial_word = partial_word.split("").each_with_index.map {|x, i| guess == word[i] ? guess : x }.join
+  elsif guess == word
+    puts "Congrats, you guessed correctly!"
+    break
   end
- else
-  puts "Word: " + "-"*word.count(word)
-  puts "Chances remaining: #{chances}"
-  puts "Guess a single letter (a-z) or the entire word:"
-  guess = gets.chomp.downcase
-  puts "Sorry, no #{guess}'s found."
-  chances -=1
- end
+  puts partial_word
+  puts "You have #{guesses} guesses left"
+  guesses -= 1
 end
+
+puts "Sorry, the word was #{word}"
+
 
 
 
